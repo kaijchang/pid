1efead48a7e8798b60d019e3b9c21b772811cc16
Kai Chang <kaijchang@gmail.com>
2019-03-24 19:53:59 -0700

    feat: steer using angle

--- demo.js
+++ demo.js
@@ -1,55 +1,98 @@
 // patch material slider to allow float step in discrete sliders
-mdc.slider.MDCSliderFoundation.prototype.setStep = eval(mdc.slider.MDCSliderFoundation.prototype.setStep.toSource().replace('||t<1', ''))
+mdc.slider.MDCSliderFoundation.prototype.setStep =
+    eval(mdc.slider.MDCSliderFoundation.prototype.setStep.toSource().replace('||t<1', ''));
 
-const pid = new PID(0.1, 0.1, 0.1);
-
-let pos, speed, size;
+let vehicle;
 
 const pSlider = new mdc.slider.MDCSlider(document.querySelector('.p-selector'));
 const iSlider = new mdc.slider.MDCSlider(document.querySelector('.i-selector'));
 const dSlider = new mdc.slider.MDCSlider(document.querySelector('.d-selector'));
 
+function PIDVehicle(pos, velocity, maxVelocity, size) {
+    this.pid = new PID(0.1, 0.1, 0.1);
+    this.angle = 0;
+
+    this.pos = pos;
+    this.velocity = velocity;
+    this.maxVelocity = maxVelocity;
+    this.size = size;
+
+    function truncate(value, max) {
+        if (value > max) {
+            return max;
+        } else if (value < -max) {
+            return - max;
+        } else {
+            return value;
+        }
+    }
+
+    this.update = function () {
+        if (mouseIsPressed &&
+            pmouseX > this.pos.x - size / 2 &&
+            pmouseX < this.pos.x + size / 2 &&
+            pmouseY > this.pos.y - size / 2 &&
+            pmouseY < this.pos.y + size / 2) {
+            this.pos.x = mouseX;
+            this.pos.y = mouseY;
+        } else {
+            this.pid.updateGains(pSlider.value, iSlider.value, dSlider.value);
+
+            const error = height / 2 - pos.y;
+
+            this.angle = this.pid.update(error);
+
+            const dRadians = this.angle / 180 * Math.PI;
+            this.velocity.add(Math.cos(dRadians), Math.sin(dRadians));
+            this.velocity.x = truncate(this.velocity.x, this.maxVelocity);
+            this.velocity.y = truncate(this.velocity.y, this.maxVelocity);
+
+            this.pos.add(this.velocity);
+
+            if (this.pos.x - size / 2 >= width) {
+                this.pos.x = 0;
+            } else if (this.pos.x + size / 2 <= 0) {
+                this.pos.x = width;
+            }
+
+            if (this.pos.y >= height - size / 2) {
+                this.pos.y = height - size / 2;
+            } else if (this.pos.y <= size / 2) {
+                this.pos.y = size / 2;
+            }
+        }
+    };
+
+    this.draw = function () {
+        push();
+
+        translate(this.pos.x, this.pos.y);
+
+        const dRadians = this.angle / 180 * Math.PI;
+
+        rotate(dRadians);
+
+        stroke(255);
+        fill(255);
+
+        rectMode(CENTER);
+        rect(0, 0, this.size, this.size);
+        pop();
+    };
+}
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    pos = createVector(0, height / 2);
 
-    speed = 1;
-    size = 50;
+    vehicle = new PIDVehicle(createVector(0, height / 2), createVector(5, 0), 5, 50);
 }
 
 function draw() {
     background(0);
     stroke(255);
     line(0, height / 2, width, height / 2);
-    rectMode(CENTER);
-    rect(pos.x, pos.y, size, size);
-
-    if (mouseIsPressed &&
-        pmouseX > pos.x - size / 2 &&
-        pmouseX < pos.x + size / 2 &&
-        pmouseY > pos.y - size / 2 &&
-        pmouseY < pos.y + size / 2) {
-        pos.x = mouseX;
-        pos.y = mouseY;
-    } else {
-        pid.updateGains(pSlider.value, iSlider.value, dSlider.value);
-
-        const error = height / 2 - pos.y;
-
-        const output = pid.update(error);
-
-        pos.add(speed, output);
-
-        if (pos.x - size / 2 >= width) {
-            pos.x = 0;
-        }
-
-        if (pos.y >= height - size / 2) {
-            pos.y = height - size / 2;
-        } else if (pos.y <= size / 2) {
-            pos.y = size / 2;
-        }
-    }
+    vehicle.update();
+    vehicle.draw();
 }
 
 function windowResized() {
--- pid.js
+++ pid.js
@@ -10,30 +10,34 @@ integral part - sum of all past errors (1 to t) multiplied by kI
 derivative part - change in error between current error (et) and last error (et-1) multiplied by kD
 */
 
-exports = typeof(exports) == 'undefined' ? window : exports;
-
-exports.PID = function PID(kP, kI, kD) {
-    this.updateGains = function (kP, kI, kD) {
+const PID = function PID(kP, kI, kD, maxI) {
+    this.updateGains = function (kP, kI, kD, maxI) {
         this.kP = kP || 0;
         this.kI = kI || 0;
         this.kD = kD || 0;
+        this.maxI = maxI || 1000;
     };
 
     this.update = function (error) {
         error = error || 0;
 
-        this.sumI += error;
-
         const P = error * this.kP;
         const I = this.sumI * this.kI;
         const D = (error - this.previousE) * this.kD;
+
+        this.sumI += I;
+        if (this.sumI > this.maxI) {
+            this.sumI = maxI;
+        } else if (this.sumI < -this.maxI) {
+            this.sumI = -maxI;
+        }
 
         this.previousE = error;
 
         return P + I + D;
     };
 
-    this.updateGains(kP, kI, kD);
+    this.updateGains(kP, kI, kD, maxI);
 
     this.previousE = 0;
     this.sumI = 0;
